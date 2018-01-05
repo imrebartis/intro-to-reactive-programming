d11fbb26a55098fc19dee279beec5ee425cfa90e
Imre Bartis <imre.bartis@siili.com>
2018-01-05 10:42:57 +0200

    use the cached network response to replace a suggested user

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-// Issue here is that we must remember that we want to specify
-// the dynamic behavior of the value(suggestions) completely at time of declaration
-// Use .startWith operator with createSuggestionStream to accomplish this goal
-// and use .merge mapped to null to take care of the refresh
 var refreshButton = document.querySelector('.refresh');
+var closeButton1 = document.querySelector('.close1');
+var closeButton2 = document.querySelector('.close2');
+var closeButton3 = document.querySelector('.close3');
 
-// Represent clicks from the refresh link as an event stream
 var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+var close1Clicks = Rx.Observable.fromEvent(closeButton1, 'click');
+var close2Clicks = Rx.Observable.fromEvent(closeButton2, 'click');
+var close3Clicks = Rx.Observable.fromEvent(closeButton3, 'click');
 
-// Initializes our page with users
 var startupRequestStream = Rx.Observable.just('https://api.github.com/users');
 
 var requestOnRefreshStream = refreshClickStream
@@ -18,27 +18,39 @@ var requestOnRefreshStream = refreshClickStream
     return 'https://api.github.com/users?since=' + randomOffset;
   });
 
-// Works for initial request stream and refresh
-var responseStream = startupRequestStream.merge(requestOnRefreshStream)
+var requestStream = startupRequestStream.merge(requestOnRefreshStream);
+
+var responseStream = requestStream
   .flatMap(requestUrl =>
     Rx.Observable.fromPromise(jQuery.getJSON(requestUrl))
-  );
-
-function createSuggestionStream(responseStream) {
-  return responseStream.map(listUser =>
-    listUser[Math.floor(Math.random()*listUser.length)]
   )
-  .startWith(null) // clearing the element at start up
-  .merge(refreshClickStream.map(ev => null)); // clearing the element at refresh
+  .shareReplay(1);
+
+// refreshClickStream: -------f------------->
+// requestStream:      r------r------------->
+// responseStream:     ---R-------R--------->
+// closeClickStream:   ---------------x----->
+// suggestion1Stream:  N--u---N---u---u----->
+
+function getRandomUser(listUsers) {
+  return listUsers[Math.floor(Math.random()*listUsers.length)];
 }
 
-var suggestion1Stream = createSuggestionStream(responseStream);
-var suggestion2Stream = createSuggestionStream(responseStream);
-var suggestion3Stream = createSuggestionStream(responseStream);
+function createSuggestionStream(responseStream, closeClickStream) {
+  return responseStream.map(getRandomUser)
+    .startWith(null)
+    .merge(refreshClickStream.map(ev => null))
+    .merge(
+      closeClickStream.withLatestFrom(responseStream, 
+                                  (x, R) => getRandomUser(R))
+    );
+}
+
+var suggestion1Stream = createSuggestionStream(responseStream, close1Clicks);
+var suggestion2Stream = createSuggestionStream(responseStream, close2Clicks);
+var suggestion3Stream = createSuggestionStream(responseStream, close3Clicks);
 
 // Rendering ---------------------------------------------------
-// The function is modified to accept null so that the element 
-// is hidden if there is no data
 function renderSuggestion(suggestedUser, selector) {
   var suggestionEl = document.querySelector(selector);
   if (suggestedUser === null) {
